3d64874d2374aab97f695d0e127a17b9498099ab
Thomas Harris <teh019283@gmail.com>
2019-10-08 12:33:18 +0200

    [buildSrc] just noticed my buildSrc code was excluded...
    Anyways this rule tweak seems to work, also this commit was supposed
    to feature release signing but since that change was in ClawPlugin
    and the whole thing just got added I'm not sure it matters

---
+++ buildSrc/src/main/java/dev/thomasharris/build/ClawPlugin.kt
@@ -0,0 +1,141 @@
+package dev.thomasharris.build
+
+import com.android.build.gradle.BaseExtension
+import com.android.build.gradle.api.AndroidBasePlugin
+import org.gradle.api.JavaVersion
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.plugin.KaptExtension
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.Properties
+
+/**
+ * Technique borrowed from https://quickbirdstudios.com/blog/gradle-kotlin-buildsrc-plugin-android/
+ */
+open class ClawPlugin : Plugin<Project> {
+    override fun apply(target: Project) = target.run {
+        apply {
+            plugin("java-library")
+            plugin("kotlin")
+            plugin("kotlin-kapt")
+        }
+
+        configureKotlin()
+        configureDependencies()
+    }
+}
+
+open class ClawAndroidPlugin : Plugin<Project> {
+    override fun apply(target: Project) = target.run {
+        apply {
+            if (!hasAndroid())
+                plugin("com.android.library")
+
+            plugin("kotlin-android")
+            plugin("kotlin-android-extensions")
+
+            plugin("kotlin-kapt")
+        }
+
+        configureKotlin()
+        configureAndroid()
+        configureDependencies()
+    }
+
+}
+
+internal fun Project.hasAndroid() = plugins.any { it is AndroidBasePlugin }
+
+internal fun Project.configureAndroid() = this.extensions.getByType<BaseExtension>().run {
+    compileSdkVersion(29)
+    defaultConfig {
+        minSdkVersion(23)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    signingConfigs {
+        create("release") {
+            val props = Properties().apply {
+                load(rootProject.file("keystore.properties").inputStream())
+            }
+            // fine with this all crashing if the types are wrong
+            storeFile = rootProject.file(props["storeFile"]!!)
+            storePassword = props["storePassword"] as String
+            keyAlias = props["keyAlias"] as String
+            keyPassword = props["keyPassword"] as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    dependencies.add("implementation", "androidx.core:core-ktx:1.1.0")
+}
+
+/**
+ * Dependencies that everyone has, guaranteed (kotlin, dagger)
+ */
+internal fun Project.configureDependencies() {
+    extensions.getByType<KaptExtension>().run {
+        javacOptions {
+            option("-Adagger.gradle.incremental")
+        }
+    }
+
+    dependencies.run {
+        add("implementation", "org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
+        add("implementation", "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
+        add("implementation", "com.google.dagger:dagger:2.22.1")
+        add("kapt", "com.google.dagger:dagger-compiler:2.22.1")
+    }
+}
+
+internal fun Project.configureKotlin() = tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+fun DependencyHandler.testing() {
+    add("testImplementation", "junit:junit:4.12")
+}
+
+fun DependencyHandler.androidTesting() {
+    add("androidTestImplementation", "androidx.test:runner:1.2.0")
+    add("androidTestImplementation", "androidx.test.espresso:espresso-core:3.2.0")
+}
+
+fun DependencyHandler.conductor() {
+    add("implementation", "com.bluelinelabs:conductor:3.0.0-rc1")
+    add("implementation", "com.bluelinelabs:conductor-archlifecycle:3.0.0-rc1")
+}
+
+fun DependencyHandler.material() {
+    add("implementation", "androidx.appcompat:appcompat:1.1.0")
+    add("implementation", "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-alpha02")
+    add("implementation", "com.google.android.material:material:1.1.0-alpha10")
+    add("implementation", "androidx.constraintlayout:constraintlayout:1.1.3")
+}
+
+fun DependencyHandler.coil() {
+    add("implementation", "io.coil-kt:coil:0.7.0")
+}
---
+++ buildSrc/src/main/java/dev/thomasharris/build/NewModuleTask.kt
@@ -0,0 +1,81 @@
+package dev.thomasharris.build
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.options.Option
+import org.jetbrains.kotlin.gradle.internal.ensureParentDirsCreated
+import java.io.File
+
+@Suppress("UnstableApiUsage")
+open class NewModuleTask : DefaultTask() {
+
+    /**
+     * module name may have dashes
+     */
+    @Input
+    @set:Option(option = "name", description = "the name of the module to create")
+    var moduleName: String = ""
+
+    @Input
+    @set:Option(option = "android", description = "Enable the android library pluging")
+    var isAndroid: Boolean = false
+
+    @Input
+    @set:Option(option = "package", description = "The package structure to initialize with")
+    var packageStructure: String = ""
+
+    @TaskAction
+    fun run() {
+        if (moduleName.isBlank()) throw GradleException("Please provide a name by doing --name=module-name")
+        if (packageStructure.isBlank()) throw GradleException("Please provide a package structure by doing --package=dev.thomasharris.lib.MyClass")
+
+        println("Module: $moduleName")
+        println("Path: ${project.rootDir.absolutePath}")
+        println("Is android? $isAndroid")
+        println("package: ${packageStructure.split(".")}")
+
+        // create the starting class
+        val ktName =
+            "$moduleName/src/main/java/${packageStructure.split(".").joinToString(separator = "/")}.kt"
+        with(File(project.rootDir, ktName)) {
+            ensureParentDirsCreated()
+            createNewFile()
+            writeText("package ${packageStructure.split(".").dropLast(1).joinToString(separator = ".")}")
+        }
+
+        // create the res folder if hasAndroid
+        if (isAndroid)
+            File(project.rootDir, "$moduleName/src/main/res").mkdirs()
+
+        // create test directory
+        val testDir =
+            "$moduleName/src/test/java/${packageStructure.split(".").dropLast(1).joinToString(
+                separator = "/"
+            )}"
+        File(project.rootDir, testDir).mkdirs()
+
+        // create manifest if hasAndroid
+        if (isAndroid) with(File(project.rootDir, "$moduleName/src/main/AndroidManifest.xml")) {
+            ensureParentDirsCreated()
+            createNewFile()
+            writeText(
+                "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+                        "    package=\"${packageStructure.split(".").dropLast(1).joinToString(
+                            separator = "."
+                        )}\" />"
+            )
+        }
+
+        // create build.gradle.kts
+        with(File(project.rootDir, "$moduleName/build.gradle.kts")) {
+            createNewFile()
+            appendText("import dev.thomasharris.build.testing\n\n")
+            appendText("plugins {\n    id(\"dev.thomasharris.claw${if (isAndroid) ".android" else ""}\")\n}\n\n")
+            appendText("dependencies {\n    testing()\n}")
+        }
+
+        File(project.rootDir, "settings.gradle.kts").appendText("\ninclude(\":$moduleName\")")
+    }
+}
