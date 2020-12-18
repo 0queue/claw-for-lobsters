737372972a88c70a808d82cc85fc1d1b7eb1aded
Thomas Harris <teh019283@gmail.com>
2020-12-18 16:28:12 +0100

    Why create magic META-INF files when you can gradle them

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,6 +8,20 @@ repositories {
     mavenCentral()
 }
 
+gradlePlugin {
+    plugins {
+        create("clawPlugin") {
+            id = "dev.thomasharris.claw"
+            implementationClass = "dev.thomasharris.claw.build.ClawPlugin"
+        }
+
+        create("clawAndroidPlugin") {
+            id = "dev.thomasharris.claw.android"
+            implementationClass = "dev.thomasharris.claw.build.ClawAndroidPlugin"
+        }
+    }
+}
+
 dependencies {
     implementation("com.android.tools.build:gradle:4.1.1")
     // CANNNOT UPDATE UNTIL kotlin-dsl PLUGIN UPDATES
